a9dbf5a5bd1e4ca0846e14caf33ddd79a6a87046
free_fish <kuailehailiyu@gmail.com>
2016-07-28 17:52:34 +0800

    learn 02

--- views.py
+++ views.py
@@ -6,5 +6,21 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/speakers')
+def speakers():
+    return render_template('speakers.html')
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
+@app.route('/venue')
+def venue():
+    return render_template('venue.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
